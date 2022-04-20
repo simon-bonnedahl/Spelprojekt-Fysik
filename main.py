bc026c7d0fa9c441a6628526ae3f90d81da6f09e
CheQita <simon2.bonnedahl@gmail.com>
2022-04-20 10:16:43 +0200

    Template

---
+++ main.py
@@ -0,0 +1,63 @@
+import pygame as pg
+from settings import *
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.fps = FPS
+
+
+
+    def new(self):
+        self.allObjects = pg.sprite.Group()
+
+    def run(self):
+        self.events()
+        self.update()
+        self.draw()
+        self.clock.tick(self.fps)
+
+
+
+
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+
+
+    def update(self):
+        for object in self.allObjects:
+            object.update()
+
+
+
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.allObjects.draw(self.screen)
+        pg.display.flip()
+
+
+    def drawText(self, text, font_name, size, color, x, y, align="center"):
+        font = pg.font.SysFont(font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect(**{align: (x, y)})
+        self.screen.blit(text_surface, text_rect)
+
+    def quit(self):
+        pg.quit()
+
+
+
+g = Game()
+g.new()
+
+while True:
+    g.run()
